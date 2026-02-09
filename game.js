750add3fd64a0c6e2232cb630267aeea288e3277
solorecord81993 <solorecord81993@gmail.com>
2026-02-09 17:04:36 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,15 +1,18 @@
 import { Car } from './car.js';
 import { updatePhysics } from './physics.js';
 
-Cesium.Ion.defaultAccessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5NmYzOWVmYy1jYWM5LTQ4MGEtYWZjMC03MGVmYzFkYzg4ZWUiLCJpZCI6Mzg4NzA5LCJpYXQiOjE3NzA2Mjk0MTl9.MDXC-Ll9x0V6LxH10HMwMzapqTjHDrj0Dv1oANR5Ugk";
+Cesium.Ion.defaultAccessToken = "PUT_YOUR_TOKEN";
 
 const viewer = new Cesium.Viewer("cesiumContainer", {
     terrain: Cesium.Terrain.fromWorldTerrain(),
     shouldAnimate: true
 });
 
-// OSM Buildings (ฟรี)
 viewer.scene.primitives.add(Cesium.createOsmBuildings());
+
+let keys = {};
+window.addEventListener("keydown", e => keys[e.key.toLowerCase()] = true);
+window.addEventListener("keyup", e => keys[e.key.toLowerCase()] = false);
 
 const lon = 100.5018;
 const lat = 13.7563;
@@ -17,9 +20,9 @@ const lat = 13.7563;
 const carto = Cesium.Cartographic.fromDegrees(lon, lat);
 
 Cesium.sampleTerrainMostDetailed(viewer.terrainProvider, [carto]).then((updated) => {
-    const height = updated[0].height;
 
-    const startPosition = Cesium.Cartesian3.fromDegrees(lon, lat, height + 1);
+    const height = updated[0].height;
+    const startPosition = Cesium.Cartesian3.fromDegrees(lon, lat, height + 2);
 
     const car = new Car(viewer, startPosition);
 
@@ -32,30 +35,10 @@ Cesium.sampleTerrainMostDetailed(viewer.terrainProvider, [carto]).then((updated)
         }
     });
 
-    viewer.clock.onTick.addEventListener((clock) => {
+    // เกมเริ่มทำงาน "หลังจากรถถูกสร้างเท่านั้น"
+    viewer.clock.onTick.addEventListener(() => {
         updatePhysics(car, keys, viewer.scene);
         car.updateCamera(viewer);
         document.getElementById("speed").innerText = car.speed.toFixed(1);
     });
-});
-
-
-viewer.camera.flyTo({
-    destination: startPosition,
-    orientation: {
-        heading: 0,
-        pitch: Cesium.Math.toRadians(-30),
-        roll: 0
-    }
-});
-
-
-let keys = {};
-window.addEventListener("keydown", e => keys[e.key.toLowerCase()] = true);
-window.addEventListener("keyup", e => keys[e.key.toLowerCase()] = false);
-
-viewer.clock.onTick.addEventListener((clock) => {
-    updatePhysics(car, keys, viewer.scene);
-    car.updateCamera(viewer);
-    document.getElementById("speed").innerText = car.speed.toFixed(1);
 });
